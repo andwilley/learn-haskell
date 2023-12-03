3b469a53df15a35eda117c351bad6f6c25e04fe6
Drew Willey <andwilley@gmail.com>
2023-12-03 18:00:50 +0000

    part two

--- aoc_2023/day1/Main.hs
+++ aoc_2023/day1/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Data.Char
+import Data.Map
 
 main :: IO ()
 main = do
@@ -8,10 +9,44 @@ main = do
   print $ doWork $ lines content
 
 doWork :: [String] -> Int
-doWork ls = sum $ map (getCodes '0' '0' False) ls
+doWork ls = sum $ fmap (getCodes '0' '0' False) ls
 
 getCodes :: Char -> Char -> Bool -> String -> Int
 getCodes first last seen [] = read [first, last]
 getCodes first last seen (x:xs) =
   if isDigit x then getCodes f x True xs else getCodes first last seen xs
   where f = if seen then first else x
+
+-- numTrie :: Map String (Maybe (Map Stri uug )
+
+newtype Trie = Trie (Map Char (Maybe Trie))
+
+newtype Tb = Tb ([(Char, Tb)])
+
+numTrie :: Trie
+numTrie = mt $ Tb [
+              ('o', Tb [('n', Tb [('e', Tb [])])]),
+              ('t',
+                Tb [
+                  ('w', Tb [('o', Tb [])]),
+                  ('h', Tb [('r', Tb [('e', Tb [('e', Tb [])])])])
+                ]),
+              ('f', Tb [
+                  ('o', Tb [('u', Tb [('r', Tb [])])]),
+                  ('i', Tb [('v', Tb [('e', Tb [])])])
+              ]),
+              ('s', Tb [
+                ('i', Tb [('x', Tb [])]),
+                ('e', Tb [('v', Tb [('e', Tb [('n', Tb [])])])])
+              ]),
+              ('e', Tb [('i', Tb [('g', Tb [('h', Tb [('t', Tb [])])])])]),
+              ('n', Tb [('i', Tb [('n', Tb [('e', Tb [])])])])
+            ]
+
+mt :: Tb -> Trie
+-- Last item in the nested list, return a map of that char to Nothing
+mt (Tb items) = Prelude.foldl buildMap (Trie empty) items
+
+buildMap :: Trie -> (Char, Tb) -> Trie
+buildMap (Trie m) (char, (Tb [])) = Trie $ insert char Nothing m
+buildMap (Trie m) (char, Tb ls) = Trie $ insert char (Just (Prelude.foldl buildMap (Trie empty) ls)) m
