4c2e4a3649faf72f6ad36152ca919c1d8532f005
Drew Willey <andwilley@gmail.com>
2023-12-03 16:32:36 +0000

    format

--- aoc_2023/day1/Main.hs
+++ aoc_2023/day1/Main.hs
@@ -12,5 +12,6 @@ doWork ls = sum $ map (getCodes '0' '0' False) ls
 
 getCodes :: Char -> Char -> Bool -> String -> Int
 getCodes first last seen [] = read [first, last]
-getCodes first last seen (x:xs) = if isDigit x then getCodes f x True xs else getCodes first last seen xs
+getCodes first last seen (x:xs) =
+  if isDigit x then getCodes f x True xs else getCodes first last seen xs
   where f = if seen then first else x
